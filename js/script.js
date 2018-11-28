4b2b8f27d0bf71e697d44a2f339ba46447905414
Albert Omware <aomware@gmail.com>
2018-11-28 05:16:54 +0300

    added some comments in the javascript code

--- js/script.js
+++ js/script.js
@@ -4,10 +4,10 @@ var b;
 var c;
 }
 //All sides are equal
-if(a==b && b==c){
+if(a=b && b=c){
   alert("equilateral triangle");
 }//At least two sides are equal
-else if(a==b && b!=c ) || (a!=b && c==a) || (c==b && c!=a){
+else if(a=b && b!=c ) || (a!=b && c=a) || (c=b && c!=a){
   alert("isosceles");
 }//No sides are equal
 else if(a!=b && b!=c && c!=a){
