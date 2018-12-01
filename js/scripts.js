d7e52038aed05d86182a063d682f3c1beac07d3b
Albert Omware <aomware@gmail.com>
2018-12-01 07:40:59 +0300

    add new stylesheet modified the code and readme file

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,9 @@ $(document).ready(function() {
   $("form#triangle").submit(function(event){
     event.preventDefault();
 
-    var side1 = parseInt($("#side1").val());
-    var side2 = parseInt($("#side2").val());
-    var side3 = parseInt($("#side3").val());
+    var side1 = parseFloat($("#side1").val());
+    var side2 = parseFloat($("#side2").val());
+    var side3 = parseFloat($("#side3").val());
     var result;
 
     if (side1 + side2 <= side3 || side2 + side3 <= side1 || side1 + side3 <= side2) {
@@ -26,4 +26,37 @@ $(document).ready(function() {
 
 
   });
+});
+$(function() {
+    var createAllErrors = function() {
+        var form = $(this);
+        var errorList = $('ul.errorMessages', form);
+
+        var showAllErrorMessages = function() {
+            errorList.empty();
+
+            //Find all invalid fields within the form.
+            form.find(':invalid').each(function(index, node) {
+
+                //Find the field's corresponding label
+                var label = $('label[for=' + node.id + ']');
+
+                //Opera incorrectly does not fill the validationMessage property.
+                var message = node.validationMessage || 'Invalid value.';
+                errorList
+                    .show()
+                    .append('<li><span>' + label.html() + '</span> ' + message + '</li>');
+            });
+        };
+
+        $('input[type=submit], button', form).on('click', showAllErrorMessages);
+        $('input[type=text]', form).on('keypress', function(event) {
+            //keyCode 13 is Enter
+            if (event.keyCode == 13) {
+                showAllErrorMessages();
+            }
+        });
+    };
+
+    $('form').each(createAllErrors);
 });
